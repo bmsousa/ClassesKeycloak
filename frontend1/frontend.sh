9e43edad4696af1a1d6ba13aa5c38fd6d27b5db9
Bruno Sousa <bmsousa@dei.uc.pt>
2024-04-19 09:38:13 +0100

    Add files for exercise

---
+++ frontend1/frontend.sh
@@ -0,0 +1,7 @@
+# Start Frontend
+
+
+# Credits to: https://github.com/PacktPublishing/Keycloak---Identity-and-Access-Management-for-Modern-Applications-2nd-Edition
+
+docker run --rm  -p 8000:8000  -it --name frontend frontend
+
---
+++ keycloak.sh
@@ -0,0 +1,2 @@
+# Start Keycloak
+docker run --rm --name keycloak -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:24.0.2  start-dev
